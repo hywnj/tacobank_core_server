cda62d00b9a63a608bbef171ab7b15b49a4685d0
hyewon <jhjsjym@gmail.com>
2024-11-11 15:35:13 +0900

    Up HealthCheckController Package

--- src/main/java/com/almagest_dev/tacobank_core_server/presentation/controller/HealthCheckController.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/presentation/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.almagest_dev.tacobank_core_server.common.controller;
+package com.almagest_dev.tacobank_core_server.presentation.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
