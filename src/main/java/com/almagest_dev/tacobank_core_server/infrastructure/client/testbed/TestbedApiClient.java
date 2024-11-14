9417112a0f0883ec869a361f6fca7934a048f2d9
hyewon <jhjsjym@gmail.com>
2024-11-14 16:39:20 +0900

    Feature Testbed 요청 메서드, Exception

--- src/main/java/com/almagest_dev/tacobank_core_server/infrastructure/client/testbed/TestbedApiClient.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/infrastructure/client/testbed/TestbedApiClient.java
@@ -1,9 +1,38 @@
 package com.almagest_dev.tacobank_core_server.infrastructure.client.testbed;
 
+import com.almagest_dev.tacobank_core_server.common.exception.TestbedApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class TestbedApiClient {
+    private final String TESTBED_API_URL = "https://almagest.io/fintech/api";
+    private final TestbedApiUtil testbedApiUtil;
 
+    /**
+     * Testbed API 요청
+     */
+    public <T, R> R  requestApi(T requestBody, String path, Class<R> responseType) {
+        // Set Request Entity (+Header)
+        HttpEntity<?> requestEntity = new HttpEntity<>(requestBody, testbedApiUtil.createHeaders());
+
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            R response = restTemplate.postForObject(
+                    TESTBED_API_URL + path,
+                    requestEntity,
+                    responseType
+            );
+
+            return response;
+
+        } catch (HttpServerErrorException ex) {
+            throw new TestbedApiException(ex.getMessage(), ex);
+        }
+    }
 
 }
