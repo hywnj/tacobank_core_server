d67c4e018036b9203901cd7eb3c4ab3883e6d84d
Ji Yun Han <jiyunhan@Jiui-MacBookAir.local>
2024-11-25 12:15:44 +0900

    up(주석 제거)

--- src/main/java/com/almagest_dev/tacobank_core_server/presentation/controller/SettlementController.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/presentation/controller/SettlementController.java
@@ -64,44 +64,5 @@ public class SettlementController {
         return ResponseEntity.ok(new CoreResponseDto<>("success", "사용자 출금 가능 계좌 잔액조회 성공", response));
     }
 
-//    // 송금 요청하기
-//    @PostMapping("/request")
-//    public ResponseEntity<List<NotificationResponseDto>> requestSettlement(@RequestBody SettlementRequestDto request) {
-//        List<NotificationResponseDto> response = settlementService.processSettlementRequest(request);
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    // 정산 현황 조회
-//    @GetMapping("/status/{memberId}")
-//    public ResponseEntity<SettlementStatusResponseDto> getSettlementStatus(@PathVariable Long memberId) {
-//        SettlementStatusResponseDto settlementStatus = settlementService.getSettlementStatus(memberId);
-//        return ResponseEntity.ok(settlementStatus);
-//    }
-//
-//    // 정산 상세내역 조회
-//    @GetMapping("/{settlementId}/details/{memberId}")
-//    public ResponseEntity<SettlementDetailsListResponseDto> getSettlementDetails(
-//            @PathVariable Long settlementId,
-//            @PathVariable Long memberId
-//    ) {
-//        SettlementDetailsListResponseDto response = settlementService.getSettlementDetails(settlementId, memberId);
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    // 독촉 알림 보내기
-//    @PostMapping("/{settlementId}/notify/{memberId}")
-//    public ResponseEntity<NotificationResponseDto> notifyPendingSettlementForMember(
-//            @PathVariable Long settlementId,
-//            @PathVariable Long memberId
-//    ) {
-//        NotificationResponseDto response = settlementService.notifyPendingSettlementForMember(settlementId, memberId);
-//        return ResponseEntity.ok(response);
-//    }
-//
-//    @PostMapping("/transfers")
-//    public ResponseEntity<?> validateSettlementsAndGetAvailableBalances(@RequestBody @Valid SettlementTransferRequestDto requestDto) {
-//        SettlementTransferResponseDto response = settlementService.validateSettlementsAndGetAvailableBalances(requestDto);
-//        return ResponseEntity.ok(new CoreResponseDto<>("success", "사용자 출금 가능 계좌 잔액조회 성공", response));
-//    }
 
 }
