5dbdd39dc462f71e3a15f2c000bf064bceabc143
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-12-02 04:09:44 +0900

    up/conf

--- src/main/java/com/almagest_dev/tacobank_core_server/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/infrastructure/config/SecurityConfig.java
@@ -17,8 +17,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-//@EnableWebSecurity
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
+//@EnableWebSecurity(debug = true)
 public class SecurityConfig {
     private final JwtProvider jwtProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
