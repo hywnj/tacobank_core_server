961aaaf2ad79e063cf8e6f2cfe11d70fe5e4348f
hyewon <jhjsjym@gmail.com>
2024-12-07 13:48:06 +0900

    Up 문자 인증 요청 Authentication 확인 추가

--- src/main/java/com/almagest_dev/tacobank_core_server/common/constants/RedisKeyConstants.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/common/constants/RedisKeyConstants.java
@@ -5,7 +5,7 @@ public class RedisKeyConstants { // Redis Key 상수
     public static final String SMS_KEY_PREFIX = "sms:verification:";
     public static final String SMS_FAILURE_PREFIX = "sms:failures:";
     public static final String SMS_SUCCESS_PREFIX = "sms:success:";
-    public static final String SMS_REQUEST_CNT_PREFIX = "sms:request";
+    public static final String SMS_REQUEST_CNT_PREFIX = "sms:request:";
     public static final String PIN_FAILURE_PREFIX = "pin:failures:";
 
     private RedisKeyConstants() {
