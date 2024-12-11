01d3da58403d4f337d819467bd2fc8f85cdd1645
hyewon <jhjsjym@gmail.com>
2024-12-12 00:05:48 +0900

    up 즐겨찾기, 최근, 친구계좌 수정

--- src/main/java/com/almagest_dev/tacobank_core_server/presentation/dto/transfer/TransferOptionsResponseDto.java
+++ src/main/java/com/almagest_dev/tacobank_core_server/presentation/dto/transfer/TransferOptionsResponseDto.java
@@ -1,6 +1,6 @@
 package com.almagest_dev.tacobank_core_server.presentation.dto.transfer;
 
-import com.almagest_dev.tacobank_core_server.presentation.dto.account.AccountDto;
+import com.almagest_dev.tacobank_core_server.presentation.dto.account.Account2Dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,7 +9,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class TransferOptionsResponseDto {
-    private List<AccountDto> favoriteAccounts;  // 즐겨찾는 계좌 리스트
-    private List<AccountDto> recentAccounts;    // 최근 이체 계좌 리스트
-    private List<AccountDto> friendsAccounts;   // 친구 메인 계좌 리스트
+    private List<Account2Dto> favoriteAccounts;  // 즐겨찾는 계좌 리스트
+    private List<Account2Dto> recentAccounts;    // 최근 이체 계좌 리스트
+    private List<Account2Dto> friendsAccounts;   // 친구 메인 계좌 리스트
 }
